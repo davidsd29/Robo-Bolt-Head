2087fc2aa900a2ab0fc70785f6732be2703d7f59
davidsd29 <davidsdankwah@gmail.com>
2023-02-24 18:57:04 +0100

    Robo Bolt head Completed

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+const ranges = document.querySelectorAll('[type=range]');
+let head = document.querySelector('head');
+
+let i = 0;
+while (i < ranges.length) {
+	ranges[i].addEventListener('input',function(){
+		slide(this);
+	});
+	// also set attributes onload of page
+	slide(ranges[i]);
+	i++;
+}
+
+function slide(s){
+	let sliderValue = s.value;
+	let sliderID = s.id;
+
+	document.body.style.setProperty('--eye'+sliderID+'-value',sliderValue+'%');
+	// head.setAttribute('data-'+sliderID+'-value',sliderValue);
+}
